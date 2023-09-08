69097a282a1ca3967b23dd1481be1cbde1ffb559
antoxl9 <67064090+antoxl9@users.noreply.github.com>
2023-09-08 17:00:53 +0200

    feat: implemented spark connect

--- spark/code/graph2.py
+++ spark/code/graph2.py
@@ -21,10 +21,6 @@ import networkx as nx  #3.0 (also pygraphviz==1.10)
 ## for timeline
 import dateparser #1.1.7
 
-txt = "black hole Stars may have been the largest stars that ever existed they burned brighter than galaxies and were larger than any Star today or that could ever exist in the future but besides their scale what makes them special and weird is that deep inside they were occupied by a cosmic parasite an endlessly hungry black hole how is that even possible foreign black hole Stars take the weirdness of black holes and go beyond to break everything we know about how stars form and grow they were only possible during a short window of time in the early universe but if they existed they would solve one of the largest mysteries of cosmology black hole stars were excessive anywhere you look at them the most massive stars today may have about 300 solar masses a black hole Star had up to 10 million solar masses of nearly pure hydrogen lets take a moment to look at what this means visually the sun wesen Al pegasi the largest star and finally the black hole star its scale is beyond words over 800 000 times wider than our sun 380 times larger than the largest star we know today and Far Below its surface is a black hole growing rapidly as it devours billions upon billions of tons of matter per second normally Stars Are Born from gigantic clouds collections of thousands or millions of solar masses of mostly hydrogen in these clouds Mata starts to accumulate around the densest spots inside as these spots get denser their gravitational pull intensifies and they grow faster eventually they generate so much heat and pressure that they ignite Fusion reactions and a new star is born but this puts a limit on their size nuclear fusion releases enough radiation energy that the surrounding gas cloud is blown away the new baby star cant gather more mass from now on the star is living on the edge between two forces gravity pulling in trying to squash the star and radiation created by Fusion pushing outwards trying to blow the star apart after millions or billions of years the core runs out of fuel and the balance breaks destroying the star but black hole stars were very very different the beasts of the other universe few hundred million years after the big bang when the universe was much smaller all the matter in existence was much more concentrated the universe was much denser and hotter Dark Matter was a dominant player forming giant structures called Dark Matter halos these dark matter Halos were so massive that they pulled in and concentrated unimaginably gigantic amounts of hydrogen gas becoming the birthplaces of the first stars and galaxies epic clouds of hydrogen fought some as massive as 100 Million Suns more than the mass of small galaxies in this unique environment that will never exist again the enormous gravitational pull of the Dark Matter Halos Drew gas into its Center and created extremely massive stars as we said before when A Star is Born it blows away the gas cloud that created it but these Titanic gas clouds in the early Universe were so large and massive that even after their birth more and more gas piled on the newborn star making it grow to unbelievable proportions the young star is forced to grow and grow and grow getting more and more massive until in some cases it reaches up to 10 million times the mass of our sun crushed by gravity its core gets hotter and hotter desperately pushing outwards trying to blow itself apart but to no avail theres too much mass and too much pressure the balance is impossible to uphold like a supernova run fast forward the core gets crushed into a black hole normally that would be the end today stars go supernova a black hole forms and things calm down but in this case the star survives its own death a tremendous explosion rocks the star from the inside but its not enough the star is so large and massive but not even a supernova can destroy it but now it has a black hole for a heart its tiny a few tens of kilometers in the center of a thing the size of the solar system the monster grows Stars Are Born from ever fastest spinning and collapsing gas and so they also spin when a black hole is born from the core of a star it keeps its angular momentum this means that matter that gets drawn in doesnt just fall in a straight line but instead begins orbiting the black hole in smaller and smaller circles going faster and faster the result is an accretion disc where gas orbits at nearly the speed of light only a small amount of gas actually falls in at any given moment basically black holes put a lot of food on the table and only nibble at it but the matter trapped in the accretion disk doesnt have a good time friction and collisions between particles heated up to temperatures of millions of degrees actively feeding black holes have accretion discs that are incredibly hot and Powerful this heat from the disc further restricts how much a black hole can devour just like the core of stars the super hot material creates radiation that blows away most of the food within its reach so even if a black hole had access to as much food as it desired it can only grow slowly a black hole embedded inside a black hole star is different the enormous pressure surrounding it pushes down matter directly into the black hole overcoming all restrictions on how fast it can consume this process is so violent and releases so much energy that the accretion disc becomes hotter and releases more radiation pressure than any star core ever could enough to push back against the weight of 10 million Suns an impossibly dangerous balance has been created millions of sodomasses pushing in the angry radiation of a force-fed black hole pushing out for the next few million years the black hole star is consumed from within the black hole grows to thousands of solar masses and the bigger it gets the faster it eats which heats up the star even more and causes it to expand in its Final Phase the black hole star has become over 30 times wider than our solar system truly the largest star to ever exist in the universe the intense magnetic fields at its core spew out Jets of plasma from the black holes poles which Pierce through the star and shoot out into space turning it into a cosmic Beacon it must have been one of the most or inducing sites to ever exist in the universe but this also marks the ant it becomes too stretched and the accretion disc within too powerful the parasite destroys its host blowing it apart a black hole with the mass of one hundred thousand suns reps its way out to hunt for new prey while leaving behind nothing but a star carcass the supermassive question if black hole Stars existed they could explain one of the greatest Mysteries of the universe the supermassive black holes we see at the center of galaxies are just too big they shouldnt be possible black holes born from regular supernovas can be a few tens of solar masses at most and because of the process we explained before they grow slowly after that if black holes merge they can form slightly larger black holes of over a hundred solar masses it should take billions and billions of years to make black holes with hundreds of thousands or even millions of solar masses and yet we know that some supermassive black holes already had 800 million solar masses only 690 million years after the big bang black hole stars are a sort of black hole cheat code if they formed very early in our universe and the black holes that emerged from them were thousands of solar masses then they could be the seeds for supermassive black holes these seeds could take root in the center of the earliest galaxies merging with others and drawing in enough matter to grow quickly and reliably very soon we may be able to verify their past existence the James Webb Space Telescope is turning its senses to explore the farthest reaches of the universe looking back in time back to the early universe that we couldnt see before so with luck we might be able to witness glimpses of these tragic Titans in the brief moment between their formation and destruction until then lets do the visual Journey again just for fun Stars a big black hole Stars bigger foreign project like the James Webb Space Telescope requires some serious budgeting but even personal finances are a nightmare to manage on your own thats why we want to introduce you to Rocket money the sponsor of todays video just as space exploration projects are planned by professionals your finances deserve professional attention too rocket money is an all-in-one Finance platform that helps with managing expenses and saving money you can use it to cancel unwanted subscriptions lower your bills create budgets monitor your credit score and create smart savings accounts the feature we like best is canceling subscriptions the platform identifies recurring charges and helps you cancel the ones you no longer want no need to go through long hold times with customer service you just click cancel within the app you can also upload screenshots of bills that you want to lower and Rocket money will get to work negotiating your bill for you join the 3.4 million members using rocket money to optimize their Finance answers and download the app today you can get access to features like budgeting for free and unlock all features with a premium subscription rocket money makes it easier than ever to spend less and save more basically managing your money doesnt have to be rocket science we know youve been waiting for a long time lets reveal the next limited edition pin its our universe to scale about 10 to the power of minus 35 seconds after the big bang the observable universe was only the size of a marble before it expanded pre-order is available for only 72 hours so you can order it now and then never again add it to your PIN collection or start a new one pin number three will be revealed soon and as the year 12022 is slowly coming to an end just kidding time flies by way too fast will take a short break to recharge our batteries thanks to all of you out there for all the love and support We Wish You a peaceful and cozy turn of the Year see you back at Full Throttle in January wait foreign"
-
-#python -m spacy download en_core_web_sm
-
 nlp = spacy.load("en_core_web_sm")
 doc = nlp(txt)
 
@@ -105,31 +101,9 @@ G = nx.from_pandas_edgelist(dtf, source="entity", target="object",
                             edge_attr="relation", 
                             create_using=nx.DiGraph())
 
-'''
-print(G)
-
-## plot
-plt.figure(figsize=(15,10))
-
-pos = nx.spring_layout(G, k=1)
-node_color = "skyblue"
-edge_color = "black"
-
-nx.draw(G, pos=pos, with_labels=True, node_color=node_color, 
-        edge_color=edge_color, cmap=plt.cm.Dark2, 
-        node_size=2000, connectionstyle='arc3,rad=0.1')
-
-nx.draw_networkx_edge_labels(G, pos=pos, label_pos=0.5, 
-                         edge_labels=nx.get_edge_attributes(G,'relation'),
-                         font_size=12, font_color='black', alpha=0.6)
-plt.show()
-'''
-
 from neo4j import GraphDatabase
 import networkx as nx
 import pandas as pd
-
-# Assuming you have a NetworkX graph called G
 
 # Convert the graph to a pandas DataFrame
 df = pd.DataFrame(G.edges(data=True), columns=['entity', 'object', 'relation'])
@@ -149,25 +123,6 @@ with driver.session() as session:
     for node in nodes:
         session.run(f"CREATE (:Node {{name: '{node}'}})")
 
-    # # Create relationships
-    # for _, row in df.iterrows():
-    #     entity = row['entity']
-    #     object = row['object']
-
-    #    # print(row['relation']['relation'])
-    #    # relation = row['relation']
-    #     relation = row['relation']['relation']  # Extract the value of the 'relation' key
-    #     #relation = relation.replace("'", "\\'")  # Escape single quotation marks
-
-          
-    #     query = (
-    #         "MATCH (n1:Node {name: $entity}), (n2:Node {name: $object})"
-    #         "CREATE (n1)-[:RELATION {type: $relation}]->(n2)"
-    #     )
-    #     session.run(f"MATCH (n1:Node {{name: '{entity}'}}), (n2:Node {{name: '{object}'}}) CREATE (n1)-[: RELATION {RELATION: '{relation}'}}]->(n2)")
-
-    #     session.run(query, entity=entity, object=object, relation=relation)
-
     cypher_queries = []
 
     # Create nodes
@@ -185,13 +140,6 @@ with driver.session() as session:
         relation = row['relation']['relation']
         relation = str(relation).upper()
 
-        # cypher_queries.append(
-        #     f"MATCH (n1:Node {{name: '{entity}'}}), (n2:Node {{name: '{object}'}}) "
-        #     f"WITH CREATE (n1)-[r:ANTONIO]->(n2)"    #r:'{relation}'
-          
-        # )
-
-        #by saro cannavo
         custom_rel_type = relation
         query = (
             "MATCH (n1:Node {name: $entity}), (n2:Node {name: $object})"
@@ -199,20 +147,6 @@ with driver.session() as session:
         )
         session.run(query, entity=entity, object=object, relation=relation, custom_rel_type=custom_rel_type)
 
-
-
-        
-
-
-    # Join all Cypher queries into a single string
-       
-        # session.run(f"MATCH (n1:Node {{name: '{entity}'}}), (n2:Node {{name: '{object}'}}) CREATE (n1)-[:{relation}]->(n2)")
-        # session.run(f"MATCH (n1:Node {{name: '{entity}'}}), (n2:Node {{name: '{object}'}}) CREATE (n1)-[:RELATION {{relation: '{relation}'}}]->(n2)")
-
-
-# Close the driver
 driver.close()
-
-#RELAZIONE NON VISUALIZZATE
 
 
--- spark/code/streaming.py
+++ spark/code/streaming.py
@@ -1,204 +1,198 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import expr
+from pyspark.sql.types import StringType, BinaryType
 from pyspark.conf import SparkConf
-from pyspark.sql.dataframe import DataFrame
 from pyspark import SparkContext
-from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json
+from pyspark.sql.types import StructType, StructField, StringType
 
-from pyspark.sql.functions import udf
+
+import spacy  #3.5.0
+from spacy import displacy
+import textacy  #0.12.0
+## for graph
+import networkx as nx  #3.0 (also pygraphviz==1.10)
 import numpy as np
-import time
-import json
-import os
-import re
+
+
+# Define a function to process the collected data
+def process_data(df):
+    # Collect the DataFrame and extract the string value
+    if df.count() > 0:
+        string_value = df.select("value").collect()[0][0]
+
+    # Pass the string_value to your non-Spark code or perform any other operations
+    extract_graph(string_value)
+
+
+def extract_graph(txt):
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(txt)
+
+    # from text to a list of sentences
+    lst_docs = [sent for sent in doc.sents]
+    print("tot sentences:", len(lst_docs))
+
+
+    # take a sentence
+    i = 1
+    lst_docs[i]
+
+
+    for token in lst_docs[i]:
+        print(token.text, "-->", "pos: "+token.pos_, "|", "dep: "+token.dep_, "")
+
+    from spacy import displacy
+
+    displacy.render(lst_docs[i], style="dep", options={"distance":100})
+
+    for tag in lst_docs[i].ents:
+        print(tag.text, f"({tag.label_})") 
+
+    displacy.render(lst_docs[i], style="ent")
+
+
+    ## extract entities and relations
+    dic = {"id":[], "text":[], "entity":[], "relation":[], "object":[]}
+
+    for n,sentence in enumerate(lst_docs):
+        lst_generators = list(textacy.extract.subject_verb_object_triples(sentence))  
+        for sent in lst_generators:
+            subj = "_".join(map(str, sent.subject))
+            obj  = "_".join(map(str, sent.object))
+            relation = "_".join(map(str, sent.verb))
+            dic["id"].append(n)
+            dic["text"].append(sentence.text)
+            dic["entity"].append(subj)
+            dic["object"].append(obj)
+            dic["relation"].append(relation)
+
+
+    ## create dataframe
+    dtf = pd.DataFrame(dic)
+    print("ma")
+    print(dic)
+    print("ma")
+
+    ## example
+    dtf[dtf["id"]==i]
+
+    ## extract attributes
+    attribute = "DATE"
+    dic = {"id":[], "text":[], attribute:[]}
+
+    for n,sentence in enumerate(lst_docs):
+        lst = list(textacy.extract.entities(sentence, include_types={attribute}))
+        if len(lst) > 0:
+            for attr in lst:
+                dic["id"].append(n)
+                dic["text"].append(sentence.text)
+                dic[attribute].append(str(attr))
+        else:
+            dic["id"].append(n)
+            dic["text"].append(sentence.text)
+            dic[attribute].append(np.nan)
+
+    dtf_att = pd.DataFrame(dic)
+    dtf_att = dtf_att[~dtf_att[attribute].isna()]
+
+    ## example
+    dtf_att[dtf_att["id"]==i]
+    print("le")
+    doc = {"entity": dtf["entity"], "target": dtf["object"], "relation": dtf["relation"] }
+    print(doc)
+    ## create full graph
+    G = nx.from_pandas_edgelist(dtf, source="entity", target="object", 
+                                edge_attr="relation", 
+                                create_using=nx.DiGraph())
+
+    from neo4j import GraphDatabase
+    import networkx as nx
+    import pandas as pd
+
+    # Convert the graph to a pandas DataFrame
+    df = pd.DataFrame(G.edges(data=True), columns=['entity', 'object', 'relation'])
+
+    # Connect to the Neo4j database
+    uri = "bolt://neo4j:7687"
+    username = "neo4j"
+    password = "password"
+
+
+    driver = GraphDatabase.driver(uri, auth=(username, password))
+
+    # Create a session
+    with driver.session() as session:
+        # Create nodes
+        nodes = set(df['entity']).union(set(df['object']))
+        for node in nodes:
+            session.run(f"CREATE (:Node {{name: '{node}'}})")
+
+        cypher_queries = []
+
+        # Create nodes
+        nodes = set(df['entity']).union(set(df['object']))
+        for node in nodes:
+            cypher_queries.append(f"CREATE (:Node {{name: '{node}'}})")
+
+        cypher_query = "\n".join(cypher_queries)
+        session.run(cypher_query)
+
+        # Create relationships
+        for _, row in df.iterrows():
+            entity = row['entity']
+            object = row['object']
+            relation = row['relation']['relation']
+            relation = str(relation).upper()
+
+            custom_rel_type = relation
+            query = (
+                "MATCH (n1:Node {name: $entity}), (n2:Node {name: $object})"
+                f"CREATE (n1)-[:{custom_rel_type} {{relation_property: $relation}}]->(n2)"
+            )
+            session.run(query, entity=entity, object=object, relation=relation, custom_rel_type=custom_rel_type)
+
+    driver.close()
 
 
 
-###new 
-## for data
-import pandas as pd  #1.1.5
-import numpy as np  #1.21.0
-
-## for plotting
-import matplotlib.pyplot as plt  #3.3.2
-
-def elaborate(batch_df: DataFrame, batch_id: int):
-  #print(batch_df)
-    batch_df.show(truncate=False)
-
-    
-    # nlp = spacy.load("en_core_web_sm")
-    # doc = nlp(batch_df)
-
-    # # from text to a list of sentences
-    # lst_docs = [sent for sent in doc.sents]
-    # print("tot sentences:", len(lst_docs))
-
-
-    # # take a sentence
-    # i = 1
-    # lst_docs[i]
-
-
-    # for token in lst_docs[i]:
-    #     print(token.text, "-->", "pos: "+token.pos_, "|", "dep: "+token.dep_, "")
-
-    # from spacy import displacy
-
-    # displacy.render(lst_docs[i], style="dep", options={"distance":100})
-
-    # for tag in lst_docs[i].ents:
-    #     print(tag.text, f"({tag.label_})") 
-
-    # displacy.render(lst_docs[i], style="ent")
-
-
-    # ## extract entities and relations
-    # dic = {"id":[], "text":[], "entity":[], "relation":[], "object":[]}
-
-    # for n,sentence in enumerate(lst_docs):
-    #     lst_generators = list(textacy.extract.subject_verb_object_triples(sentence))  
-    #     for sent in lst_generators:
-    #         subj = "_".join(map(str, sent.subject))
-    #         obj  = "_".join(map(str, sent.object))
-    #         relation = "_".join(map(str, sent.verb))
-    #         dic["id"].append(n)
-    #         dic["text"].append(sentence.text)
-    #         dic["entity"].append(subj)
-    #         dic["object"].append(obj)
-    #         dic["relation"].append(relation)
-
-
-    # ## create dataframe
-    # dtf = pd.DataFrame(dic)
-    # print("ma")
-    # print(dic)
-    # print("ma")
-
-    # ## example
-    # dtf[dtf["id"]==i]
-
-    # ## extract attributes
-    # attribute = "DATE"
-    # dic = {"id":[], "text":[], attribute:[]}
-
-    # for n,sentence in enumerate(lst_docs):
-    #     lst = list(textacy.extract.entities(sentence, include_types={attribute}))
-    #     if len(lst) > 0:
-    #         for attr in lst:
-    #             dic["id"].append(n)
-    #             dic["text"].append(sentence.text)
-    #             dic[attribute].append(str(attr))
-    #     else:
-    #         dic["id"].append(n)
-    #         dic["text"].append(sentence.text)
-    #         dic[attribute].append(np.nan)
-
-    # dtf_att = pd.DataFrame(dic)
-    # dtf_att = dtf_att[~dtf_att[attribute].isna()]
-
-    # ## example
-    # dtf_att[dtf_att["id"]==i]
-    # print("le")
-    # doc = {"entity": dtf["entity"], "target": dtf["object"], "relation": dtf["relation"] }
-    # print(doc)
-    # ## create full graph
-    # G = nx.from_pandas_edgelist(dtf, source="entity", target="object", 
-    #                             edge_attr="relation", 
-    #                             create_using=nx.DiGraph())
-
-    # '''
-    # print(G)
-
-    # ## plot
-    # plt.figure(figsize=(15,10))
-
-    # pos = nx.spring_layout(G, k=1)
-    # node_color = "skyblue"
-    # edge_color = "black"
-
-    # nx.draw(G, pos=pos, with_labels=True, node_color=node_color, 
-    #         edge_color=edge_color, cmap=plt.cm.Dark2, 
-    #         node_size=2000, connectionstyle='arc3,rad=0.1')
-
-    # nx.draw_networkx_edge_labels(G, pos=pos, label_pos=0.5, 
-    #                         edge_labels=nx.get_edge_attributes(G,'relation'),
-    #                         font_size=12, font_color='black', alpha=0.6)
-    # plt.show()
-    # '''
-
-    # from neo4j import GraphDatabase
-    # import networkx as nx
-    # import pandas as pd
-
-    # # Assuming you have a NetworkX graph called G
-
-    # # Convert the graph to a pandas DataFrame
-    # df = pd.DataFrame(G.edges(data=True), columns=['entity', 'object', 'relation'])
-
-    # # Connect to the Neo4j database
-    # uri = "bolt://localhost:7687"
-    # username = "neo4j"
-    # password = "password"
-
-
-    # driver = GraphDatabase.driver(uri, auth=(username, password))
-
-    # # Create a session
-    # with driver.session() as session:
-    #     # Create nodes
-    #     nodes = set(df['entity']).union(set(df['object']))
-    #     for node in nodes:
-    #         session.run(f"CREATE (:Node {{name: '{node}'}})")
-
-    #     # Create relationships
-    #     for _, row in df.iterrows():
-    #         entity = row['entity']
-    #         object = row['object']
-    #     # relation = row['relation']
-    #         relation = row['relation']['relation']  # Extract the value of the 'relation' key
-    #         relation = relation.replace("'", "\\'")  # Escape single quotation marks
-    #         session.run(f"MATCH (n1:Node {{name: '{entity}'}}), (n2:Node {{name: '{object}'}}) CREATE (n1)-[:RELATION {{relation: '{relation}'}}]->(n2)")
-
-    #     # session.run(f"MATCH (n1:Node {{name: '{entity}'}}), (n2:Node {{name: '{object}'}}) CREATE (n1)-[:{relation}]->(n2)")
-    #     # session.run(f"MATCH (n1:Node {{name: '{entity}'}}), (n2:Node {{name: '{object}'}}) CREATE (n1)-[:RELATION {{relation: '{relation}'}}]->(n2)")
 
 
 
-'''
-def get_spark_session():
-    spark_conf = SparkConf() \
-        .set('spark.streaming.stopGracefullyOnShutdown', 'true') \
-        .set('spark.streaming.kafka.consumer.cache.enabled', 'false') \
-        .set('spark.streaming.backpressure.enabled', 'true') \
-        .set('spark.streaming.kafka.maxRatePerPartition', '100') \
-        .set('spark.streaming.kafka.consumer.poll.ms', '512') \
-        .set('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1') \
-        .set('spark.sql.streaming.checkpointLocation', '/tmp/checkpoint')
-    return spark_conf
-'''
 
-sc = SparkContext(appName="PythonStructuredStreamsKafka")
+# Kafka configuration
+kafka_bootstrap_servers = "kafkaserver:9092"
+kafka_topic = "datitranscript"
+
+sc = SparkContext(appName="AstroKG")
 spark = SparkSession(sc)
 sc.setLogLevel("WARN")
 
-'''
-spark_session = SparkSession.builder \
-    .appName('astrokg') \
-    .config(conf=spark_conf) \
-    .getOrCreate()
-'''
-
-
-#spark = get_spark_session()
-topic = "datitranscript"
-kafkaServer = "kafkaserver:9092"
-
-# Read messages from Kafka
 df = spark \
     .readStream \
-    .format('kafka') \
-    .option('kafka.bootstrap.servers', kafkaServer) \
-    .option('subscribe', topic) \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+    .option("subscribe", kafka_topic) \
     .load()
-    
-   
+
+# query = df.writeStream \
+#     .outputMode("append") \
+#     .format("console") \
+#     .start()
+schema = StructType([
+    StructField("key", StringType(), True),
+    StructField("value", StringType(), True),
+    # Add more fields as needed
+])
+
+# Cast the message received from kafka with the provided schema
+df = df.selectExpr("CAST(value AS STRING)") \
+    .select(from_json("value", schema).alias("data")) \
+    .select("data.*")
+
+query = df.writeStream \
+    .outputMode("append") \
+    .foreachBatch(lambda batch_df, batch_id: process_data(batch_df)) \
+    .start()
+
+query.awaitTermination()
